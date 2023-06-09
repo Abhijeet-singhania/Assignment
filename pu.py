32993c81c6acf03a5abbfeb2b6ce88193b5ba2b1
Abhijeet-singhania <62837360+Abhijeet-singhania@users.noreply.github.com>
2023-06-09 12:29:29 +0530

    Update

--- pu.py
+++ pu.py
@@ -1,62 +1,124 @@
-import csv
-import requests
-from bs4 import BeautifulSoup
-import time
+from flask import Flask, jsonify, request
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.exc import SQLAlchemyError
 
-# Set the base URL and headers
-base_url = "https://www.amazon.in"
-headers = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36"
-}
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tasks.db'
+db = SQLAlchemy(app)
 
-# Initialize the CSV file and headers
-csv_file = open("amazon_products.csv", "w", newline="", encoding="utf-8")
-writer = csv.writer(csv_file)
-writer.writerow(["Product URL", "Product Name", "Product Price", "Rating", "Number of Reviews", "Description", "ASIN", "Product Description", "Manufacturer"])
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.String(255))
+    due_date = db.Column(db.Date)
+    status = db.Column(db.String(20), nullable=False)
 
-# Function to scrape product details from a product URL
-def scrape_product_details(url):
-    response = requests.get(url, headers=headers)
-    soup = BeautifulSoup(response.content, "html.parser")
+    def to_dict(self):
+        """
+        Convert the Task object to a dictionary.
+        """
+        return {
+            'id': self.id,
+            'title': self.title,
+            'description': self.description,
+            'due_date': str(self.due_date),
+            'status': self.status
+        }
 
-    # Scrape required product details
-    product_name = soup.select_one("#productTitle").get_text(strip=True)
-    product_price = soup.select_one("#priceblock_ourprice").get_text(strip=True)
-    rating = soup.select_one(".a-icon-star span").get_text(strip=True)
-    num_reviews = soup.select_one("#acrCustomerReviewText").get_text(strip=True)
-    description = soup.select_one("#productDescription").get_text(strip=True)
-    asin = soup.find("th", text="ASIN").find_next("td").get_text(strip=True)
-    product_description = soup.select_one("#feature-bullets ul").get_text(strip=True)
-    manufacturer = soup.find("th", text="Manufacturer").find_next("td").get_text(strip=True)
+@app.route('/tasks', methods=['POST'])
+def create_task():
+    try:
+        data = request.get_json()
+        title = data.get('title')
+        description = data.get('description')
+        due_date = data.get('due_date')
+        status = data.get('status')
 
-    # Write the data to the CSV file
-    writer.writerow([url, product_name, product_price, rating, num_reviews, description, asin, product_description, manufacturer])
+        # Input validation
+        if not title:
+            return jsonify({'error': 'Title is required.'}), 400
 
-# Function to scrape products from a search page
-def scrape_products(search_url, num_pages):
-    for page in range(1, num_pages + 1):
-        url = search_url + f"&page={page}"
-        response = requests.get(url, headers=headers)
-        soup = BeautifulSoup(response.content, "html.parser")
+        if status not in ['Incomplete', 'In Progress', 'Completed']:
+            return jsonify({'error': 'Invalid status value.'}), 400
 
-        # Get the product links from the search page
-        product_links = soup.select(".s-result-item .a-link-normal.a-text-normal")
-        
-        # Iterate over each product link and scrape the details
-        for link in product_links:
-            product_url = base_url + link["href"]
-            scrape_product_details(product_url)
-            
-        # Add a delay to avoid overwhelming the server
-        time.sleep(2)
+        task = Task(
+            title=title,
+            description=description,
+            due_date=due_date,
+            status=status
+        )
+        db.session.add(task)
+        db.session.commit()
 
-# Start scraping the products
-search_url = "https://www.amazon.in/s?k=bags&crid=2M096C61O4MLT&qid=1653308124&sprefix=ba%2Caps%2C283&ref=sr_pg_"
-num_pages = 20  # Scrape 20 pages of product listings
+        return jsonify({'message': 'Task created successfully.'}), 201
+    except SQLAlchemyError as e:
+        db.session.rollback()
+        return jsonify({'error': 'An error occurred while creating the task.'}), 500
 
-scrape_products(search_url, num_pages)
+@app.route('/tasks/<int:task_id>', methods=['GET'])
+def get_task(task_id):
+    task = Task.query.get(task_id)
+    if task:
+        return jsonify(task.to_dict())
+    else:
+        return jsonify({'error': 'Task not found.'}), 404
 
-# Close the CSV file
-csv_file.close()
+@app.route('/tasks/<int:task_id>', methods=['PUT'])
+def update_task(task_id):
+    try:
+        task = Task.query.get(task_id)
+        if not task:
+            return jsonify({'error': 'Task not found.'}), 404
 
-print("Scraping completed and data exported to 'amazon_products.csv'")
+        data = request.get_json()
+        title = data.get('title')
+        description = data.get('description')
+        due_date = data.get('due_date')
+        status = data.get('status')
+
+        # Input validation
+        if not title:
+            return jsonify({'error': 'Title is required.'}), 400
+
+        if status and status not in ['Incomplete', 'In Progress', 'Completed']:
+            return jsonify({'error': 'Invalid status value.'}), 400
+
+        task.title = title
+        task.description = description
+        task.due_date = due_date
+        task.status = status
+        db.session.commit()
+
+        return jsonify({'message': 'Task updated successfully.'})
+    except SQLAlchemyError as e:
+        db.session.rollback()
+        return jsonify({'error': 'An error occurred while updating the task.'}), 500
+
+@app.route('/tasks/<int:task_id>', methods=['DELETE'])
+def delete_task(task_id):
+    try:
+        task = Task.query.get(task_id)
+        if task:
+            db.session.delete(task)
+            db.session.commit()
+            return jsonify({'message': 'Task deleted successfully.'})
+        else:
+            return jsonify({'error': 'Task not found.'}), 404
+    except SQLAlchemyError as e:
+        db.session.rollback()
+        return jsonify({'error': 'An error occurred while deleting the task.'}), 500
+
+@app.route('/tasks', methods=['GET'])
+def list_tasks():
+    try:
+        page = request.args.get('page', 1, type=int)
+        per_page = request.args.get('per_page', 10, type=int)
+        tasks = Task.query.paginate(page=page, per_page=per_page)
+        task_list = [task.to_dict() for task in tasks.items]
+        return jsonify(task_list)
+    except SQLAlchemyError as e:
+        return jsonify({'error': 'An error occurred while retrieving the tasks.'}), 500
+
+if __name__ == '__main__':
+    db.create_all()
+    app.run()
